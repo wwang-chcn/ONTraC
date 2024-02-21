f45417fdcfde4e8627d282ac5954bea4489d5e4a
Wen Wang (王文) <wwang.bio@gmail.com>
2024-02-21 11:25:28 -0500

    Update: version handle

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup, find_packages
 
+
+# Function to read the version from the package's version.py
+def get_version():
+    version = {}
+    with open("ONTraC/version.py") as fp:
+        exec(fp.read(), version)
+    return version['__version__']
+
 setup(
     name='ONTraC',
-    version='1.1.2',
+    version=get_version(),
     package_dir={'ONTraC': 'ONTraC'},  # Specify the root directory for package contents
     packages=['ONTraC','ONTraC.run','ONTraC.model','ONTraC.train','ONTraC.utils','ONTraC.optparser'],  # Specify the root directory for finding packages
     # Add other package metadata (author, description, dependencies, etc.)
