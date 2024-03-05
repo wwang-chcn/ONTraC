26c1f7dd6c7187a3abf7d40d24fb07962d614b6d
Wen Wang (王文) <wwang.bio@gmail.com>
2024-03-05 15:30:41 -0500

    Remove: unused files

--- ONTraC/optparser/__init__.py
+++ ONTraC/optparser/__init__.py
@@ -1,6 +1,3 @@
 from ._create_dataset import opt_create_ds_validate, prepare_create_ds_optparser
 from ._GP import opt_GP_validate, prepare_GP_optparser
-from ._NP import opt_NP_validate, prepare_NP_optparser
-from ._GSAE import opt_GSAE_validate, prepare_GSAE_optparser
-from ._GSAP import opt_GSAP_validate, prepare_GSAP_optparser
 from ._NT import opt_NT_validate, prepare_NT_optparser
