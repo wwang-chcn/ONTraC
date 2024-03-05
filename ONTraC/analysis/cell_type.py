9d4d95819ffdc977204601ac27dd30908efd6d77
Wen Wang (王文) <wwang.bio@gmail.com>
2024-03-05 16:50:43 -0500

    Rename: NTScore & Cell_Type

--- ONTraC/analysis/cell_type.py
+++ ONTraC/analysis/cell_type.py
@@ -22,8 +22,8 @@ def cell_type_dis_in_cluster(options: Values, data: Data, meta_df: pd.DataFrame)
     soft_assign_file = f'{options.output}/consolidate_s.csv.gz'
     mask = data.mask.flatten().detach().cpu().numpy()
     soft_assign = np.loadtxt(soft_assign_file, delimiter=',')[mask]  # N x n_clusters
-    meta_df['annot'] = meta_df['annot'].astype('category')
-    cell_type = meta_df['annot']
+    meta_df['Cell_Type'] = meta_df['Cell_Type'].astype('category')
+    cell_type = meta_df['Cell_Type']
     cell_type_cat = cell_type.cat.categories
     cell_type_one_hot = to_one_hot(cell_type.cat.codes, num_classes=len(cell_type_cat))  # N x n_cell_types
 
@@ -91,10 +91,10 @@ def cell_type_dis_in_cluster(options: Values, data: Data, meta_df: pd.DataFrame)
         cell_type_dis_melt_df = pd.melt(
             cell_type_dis_df,
             id_vars='cluster',  # type: ignore
-            var_name='annot',
+            var_name='Cell_Type',
             value_vars=cell_type_cat,  # type: ignore
             value_name='Number')
-        g = sns.catplot(cell_type_dis_melt_df, kind="bar", x="Number", y="annot", col="cluster", height=4,
+        g = sns.catplot(cell_type_dis_melt_df, kind="bar", x="Number", y="Cell_Type", col="cluster", height=4,
                         aspect=.5)  # type: ignore
         g.add_legend()
         g.figure.figsize = (6.4, 20)
@@ -103,78 +103,9 @@ def cell_type_dis_in_cluster(options: Values, data: Data, meta_df: pd.DataFrame)
         g.savefig(f'{options.output}/cell_type_number_within_clusters.pdf')
 
 
-def niche_max_dis_in_cluster(options: Values, data: Data, meta_df: pd.DataFrame) -> None:
+def NTScore_in_each_cell_type(options: Values, meta_df: pd.DataFrame) -> None:
     """
-    Plot cell type distribution in each cluster.
-    """
-
-    soft_assign_file = f'{options.output}/consolidate_s.csv.gz'
-
-    mask = data.mask.flatten().detach().cpu().numpy()
-    soft_assign = np.loadtxt(soft_assign_file, delimiter=',')[mask]  # N x n_clusters
-    meta_df['annot_niche_max'] = meta_df['annot_niche_max'].astype('category')
-    cell_type = meta_df['annot_niche_max']
-    cell_type_cat = cell_type.cat.categories
-    cell_type_one_hot = to_one_hot(cell_type.cat.codes, num_classes=len(cell_type_cat))  # N x n_cell_types
-
-    cell_type_dis = np.matmul(soft_assign.T, cell_type_one_hot)  # n_clusters x n_cell_types
-    cell_type_dis_df = pd.DataFrame(cell_type_dis, columns=cell_type_cat)
-
-    # ----- gini index for intra-cluster niche type distribution -----
-    intra_cluster_gini = cell_type_dis_df.apply(gini, axis=1).values
-    intra_cluster_gini_df = pd.DataFrame(data={'gini': intra_cluster_gini, 'cluster': cell_type_dis_df.index})
-    intra_cluster_gini_df.to_csv(f'{options.output}/intra_cluster_gini_niche_type.csv', index=False)
-
-    with sns.axes_style('white', rc={
-            'xtick.bottom': True,
-            'ytick.left': True
-    }), sns.plotting_context('paper',
-                             rc={
-                                 'axes.titlesize': 14,
-                                 'axes.labelsize': 12,
-                                 'xtick.labelsize': 10,
-                                 'ytick.labelsize': 10,
-                                 'legend.fontsize': 10
-                             }):
-        # ----- heatmap for cluster × niche type -----
-        fig, ax = plt.subplots(figsize=(8, 8))
-        sns.heatmap(cell_type_dis_df.apply(lambda x: x / x.sum(), axis=1), ax=ax, cmap='Blues')
-        ax.set_title('Cell Type Distribution in Each Cluster')
-        ax.set_xlabel('Cell Type')
-        ax.set_ylabel('Cluster')
-        fig.tight_layout()
-        fig.savefig(f'{options.output}/niche_max_dis_in_clusters.pdf')
-        plt.close(fig)
-
-        fig, ax = plt.subplots(figsize=(8, 8))
-        sns.heatmap(cell_type_dis_df.apply(lambda x: x / x.sum(), axis=0), ax=ax, cmap='Blues')
-        ax.set_title('Cell Type Distribution across Clusters')
-        ax.set_xlabel('Cell Type')
-        ax.set_ylabel('Cluster')
-        fig.tight_layout()
-        fig.savefig(f'{options.output}/niche_max_dis_across_clusters.pdf')
-        plt.close(fig)
-
-        # ----- bar plot for niche type in each cluster -----
-        cell_type_dis_df['cluster'] = cell_type_dis_df.index
-        cell_type_dis_melt_df = pd.melt(
-            cell_type_dis_df,
-            id_vars='cluster',  # type: ignore
-            var_name='annot',
-            value_vars=cell_type_cat,  # type: ignore
-            value_name='Number')
-        g = sns.catplot(cell_type_dis_melt_df, kind="bar", x="annot", y="Number", col="cluster", height=4,
-                        aspect=.5)  # type: ignore
-        g.add_legend()
-        g.figure.figsize = (6.4, 20)
-        g.tight_layout()
-        g.set_xticklabels(rotation='vertical')
-        g.savefig(f'{options.output}/niche_max_number_within_clusters.pdf')
-
-
-def pseudotime_in_each_cell_type(options: Values, meta_df: pd.DataFrame) -> None:
-    """
-    Plot pseudo time in each cell type.
+    Plot NT score in each cell type.
     """
     with sns.axes_style('white', rc={
             'xtick.bottom': True,
@@ -187,11 +118,9 @@ def pseudotime_in_each_cell_type(options: Values, meta_df: pd.DataFrame) -> None
                                  'ytick.labelsize': 10,
                                  'legend.fontsize': 10
                              }):
-        fig, axes = plt.subplots(2, 1, figsize=(6, 8))
-        sns.violinplot(data=meta_df, x='annot', y='pseudo_time', ax=axes[0])
-        axes[0].set_xticklabels(axes[0].get_xticklabels(), rotation='vertical')
-        sns.violinplot(data=meta_df, x='annot_niche_max', y='pseudo_time', ax=axes[1])
-        axes[1].set_xticklabels(axes[1].get_xticklabels(), rotation='vertical')
+        fig, ax = plt.subplots(figsize=(6, 4))
+        sns.violinplot(data=meta_df, x='Cell_Type', y='NTScore', ax=ax)
+        ax.set_xticklabels(ax.get_xticklabels(), rotation='vertical')
         fig.tight_layout()
-        fig.savefig(f'{options.output}/PseudoTime_in_each_cell_type.pdf')
+        fig.savefig(f'{options.output}/NTScore_in_each_cell_type.pdf')
         plt.close(fig)
