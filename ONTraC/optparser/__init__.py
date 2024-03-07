b9d878c97320a1ea349d64049432678b3a0a5c7a
Crystal Shin <54536735+sjcshin@users.noreply.github.com>
2024-03-07 11:54:43 -0500

    Update __init__.py

--- ONTraC/optparser/__init__.py
+++ ONTraC/optparser/__init__.py
@@ -1,3 +1,4 @@
 from ._create_dataset import opt_create_ds_validate, prepare_create_ds_optparser
 from ._GP import opt_GP_validate, prepare_GP_optparser
 from ._NT import opt_NT_validate, prepare_NT_optparser
+from ._ONTraC import *
