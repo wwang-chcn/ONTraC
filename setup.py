a8ef15d5f60e33a5f03b92d88622da3d6e7642f1
Crystal Shin <54536735+sjcshin@users.noreply.github.com>
2024-03-07 12:15:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,4 +22,4 @@ setup(
     #         'gp = ONTraC.bin.GP:main'
     #     ],
     # },
-    scripts=['ONTraC/bin/createDataSet', 'ONTraC/bin/GP', 'ONTraC/bin/NTScore'])
+    scripts=['ONTraC/bin/createDataSet', 'ONTraC/bin/GP', 'ONTraC/bin/NTScore', 'ONTraC/bin/ONTraC'])
