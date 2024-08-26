bd1aadd28953cf9b43725bd39f6113c8ca261b98
Wen Wang (王文) <wwang.bio@gmail.com>
2024-08-25 20:57:00 -0400

    Update: incoperate with V2

--- tests/test_niche_trajectory.py
+++ tests/test_niche_trajectory.py
@@ -1,7 +1,18 @@
+from optparse import Values
+
 import numpy as np
+import pytest
 
 from ONTraC.niche_trajectory._niche_trajectory import (
-    get_niche_trajectory_path, trajectory_path_to_NC_score, get_niche_NTScore)
+    get_niche_NTScore, get_niche_trajectory_path, trajectory_path_to_NC_score)
+
+
+@pytest.fixture
+def options() -> Values:
+    # Create an options object for testing
+    _options = Values()
+    _options.equal_space = False
+    return _options
 
 
 def test_get_niche_trajectory_path():
@@ -11,11 +22,13 @@ def test_get_niche_trajectory_path():
     assert get_niche_trajectory_path(adj_matrix) == expected_path
 
 
-def test_trajectory_path_to_NC_score():
+def test_trajectory_path_to_NC_score(options: Values):
     # Test case: Niche trajectory path with 6 clusters
     niche_trajectory_path = [1, 2, 3, 4, 5, 0]
     expected_NT_score = np.array([1., 0., 0.2, 0.4, 0.6, 0.8])
-    gen_NT_score = trajectory_path_to_NC_score(niche_trajectory_path)
+    gen_NT_score = trajectory_path_to_NC_score(options=options,
+                                               niche_trajectory_path=niche_trajectory_path,
+                                               niche_clustering_sum=np.array([1.1, 0.8, 1.1]))
     assert np.allclose(gen_NT_score, expected_NT_score)
 
 
@@ -24,10 +37,9 @@ def test_get_niche_NTScore():
     niche_cluster_loading = np.array([[0.1, 0.2, 0.7], [0.4, 0.5, 0.1], [0.6, 0.1, 0.3]])  # #niche x #niche_cluster
     niche_adj_matrix = np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
     expected_niche_cluster_score = np.array([0, 0.5, 1])
-    expected_niche_level_NTScore = np.array([0.8 , 0.35, 0.35])
+    expected_niche_level_NTScore = np.array([0.8, 0.35, 0.35])
 
     niche_cluster_score, niche_level_NTScore = get_niche_NTScore(niche_cluster_loading, niche_adj_matrix)
 
     assert np.allclose(niche_cluster_score, expected_niche_cluster_score)
     assert np.allclose(niche_level_NTScore, expected_niche_level_NTScore)
-
