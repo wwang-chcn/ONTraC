f6f6edfed651c51b4e2d0410d0d282d874ae58ed
Crystal Shin <54536735+sjcshin@users.noreply.github.com>
2024-10-14 15:09:38 -0400

    Held-Karp implementation update
    The Held-Karp algorithm was updated to perfectly mimic the results of brute force including certain edge cases - compare against all cases of starting nodes

--- src/ONTraC/niche_trajectory/algorithm.py
+++ src/ONTraC/niche_trajectory/algorithm.py
@@ -25,68 +25,43 @@ def held_karp(conn_matrix: np.ndarray) -> List[int]:
     Held-Karp algorithm to find the optimal path with the highest connectivity.
     """
     n = conn_matrix.shape[0]
-    C = {}
+    max_connectivity = float('-inf')
+    optimal_path = []
 
     # --- Held-Karp algorithm ---
-    # Initial state
-    for k in range(1, n):
-        C[(1 << k, k)] = (conn_matrix[0][k], [0, k])
+    # Test each node as the starting node
+    for start_node in range(n):
+        C = {}
 
-    # Iterate subsets of increasing length and store the maximum path
-    for subset_size in range(2, n):
-        for subset in itertools.combinations(range(1, n), subset_size):
-            bits = 0
-            for bit in subset:
-                bits |= 1 << bit
+        # Initial state
+        for k in range(n):
+            if k != start_node:
+                C[(1 << k, k)] = (conn_matrix[start_node][k], [start_node, k])
 
-            for k in subset:
-                prev_bits = bits & ~(1 << k)
-                res = []
-                for m in subset:
-                    if m == k:
-                        continue
-                    res.append((C[(prev_bits, m)][0] + conn_matrix[m][k], C[(prev_bits, m)][1] + [k]))
-                C[(bits, k)] = max(res)
+        # Iterate subsets of increasing length and store the maximum path
+        for subset_size in range(2, n):
+            for subset in itertools.combinations(range(n), subset_size):
+                if start_node in subset:
+                    continue
+                bits = 0
+                for bit in subset:
+                    bits |= 1 << bit
 
-    # We're interested in all bits but the least significant (the start city)
-    bits = (2**n - 1) - 1
+                for k in subset:
+                    prev_bits = bits & ~(1 << k)
+                    res = []
+                    for m in subset:
+                        if m == k:
+                            continue
+                        res.append((C[(prev_bits, m)][0] + conn_matrix[m][k], C[(prev_bits, m)][1] + [k]))
+                    C[(bits, k)] = max(res)
 
-    res = []
-    for k in range(1, n):
-        res.append((C[(bits, k)][0] + conn_matrix[k][0], C[(bits, k)][1]))
-
-    max_cost, optimal_path = max(res)
-    optimal_path.append(0)  # complete the cycle
-
-    # --- Cut the shortest edge out from the cycle ---
-    dists = []
-    for i in range(len(optimal_path)-1):
-        dists.append(conn_matrix[optimal_path[i]][optimal_path[i+1]])
-
-    cut_index = dists.index(min(dists))
-    if optimal_path[cut_index] < optimal_path[cut_index + 1]:
-        start_index = cut_index
-        end_index = cut_index + 1
-    else:
-        start_index = cut_index + 1
-        end_index = cut_index
-
-    if start_index == len(optimal_path) - 1:
-        optimal_path.pop(start_index)
-    elif start_index == 0:
-        optimal_path.pop(start_index)
-        optimal_path.reverse()
-    elif start_index < end_index:
-        optimal_path.pop(len(optimal_path)-1)
-        seg1 = optimal_path[:start_index + 1]
-        seg1.reverse()
-        seg2 = optimal_path[end_index:]
-        seg2.reverse()
-        optimal_path = seg1 + seg2
-    else:
-        optimal_path.pop(len(optimal_path)-1)
-        seg1 = optimal_path[start_index:]
-        seg2 = optimal_path[:end_index + 1]
-        optimal_path = seg1 + seg2
+        # Find the best path that ends at any node excluding the start node
+        bits = (2**n - 1) - (1 << start_node)
+        for k in range(n):
+            if k != start_node and (bits, k) in C:
+                if C[(bits, k)][0] > max_connectivity:
+                    max_connectivity = C[(bits, k)][0]
+                    optimal_path = C[(bits, k)][1]
 
     return optimal_path
