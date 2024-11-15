7f84d491c85e3f060462a9836ed9f59ad2533a93
Wen Wang (王文) <wwang.bio@gmail.com>
2024-11-15 16:35:02 -0500

    Refine: logic

--- src/ONTraC/constants.py
+++ src/ONTraC/constants.py
@@ -1,10 +1,10 @@
 from typing import Dict, List
 
 IO_OPTIONS: Dict[str, List[str]] = {
-    'ONTraC': ['input', 'NN_dir', 'GNN_dir', 'NT_dir'],
-    'ONTraC_NN': ['input', 'NN_dir'],  # niche network
+    'ONTraC': ['NN_dir', 'GNN_dir', 'NT_dir', 'input'],
+    'ONTraC_NN': ['NN_dir', 'input'],  # niche network
     'ONTraC_GNN': ['NN_dir', 'GNN_dir'],  # graph neural network
     'ONTraC_NT': ['NN_dir', 'GNN_dir', 'NT_dir'],  # niche trajectory
     'ONTraC_GT': ['NN_dir', 'GNN_dir', 'NT_dir'],  # GNN & niche trajectory
-    'ONTraC_analysis': ['input', 'NN_dir', 'GNN_dir', 'NT_dir', 'output', 'log'],  # analysis
+    'ONTraC_analysis': ['NN_dir', 'GNN_dir', 'NT_dir', 'input', 'output', 'log'],  # analysis
 }
